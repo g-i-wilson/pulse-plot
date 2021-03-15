741cf9eefbd5ce74879353e2b4c6df29f3d1b844
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-03-15 17:55:23 -0500

    working 2D grid view

--- ServePulses.java
+++ ServePulses.java
@@ -33,59 +33,16 @@ public class ServePulses {
 
 
 class PulseServerState extends ServerState {
-
-  private List<List<Integer>> pulses;
-  private List<String> pulseTimes;
-  private File dataFile;
   
+  private TemplateFile plotlyTemplate;
+  private TemplateFile plotlyJS;
   private Database pulseDatabase;
   
-  public PulseServerState () throws Exception {
-  	this(
-			LocalDateTime.now().format(
-		  	DateTimeFormatter.ofPattern("dd-MM-yyyy_HHmmss")
-		  )+".csv"
-    );
-  }
-
   public PulseServerState ( String folderName ) throws Exception {
-  	String tableName = LocalDateTime.now().format( DateTimeFormatter.ofPattern("dd-MM-yyyy_HHmmss") );
-  	pulses = new ArrayList<>();
-  	pulseTimes = new ArrayList<>();
-//  	Files.write(dataFile.toPath(), "".getBytes());
+		plotlyTemplate = new TemplateFile( "plot-template.html", "////" );
+		plotlyJS = new TemplateFile( "plotly-latest.min.js", "////" );
   	pulseDatabase = new Database( folderName );
     System.out.println( "PulseServerState initialized!" );
-  }
-  
-  public void addDataCSV ( String data ) {
-  	System.out.println( data );
-  	String dataPoints[];
-  	//if (data.equals("") || data.equals("data=")) return;
-  	try {
-  		dataPoints = data.split("=")[1].split("%2C");
-  	} catch (Exception e) {
-  		return;
-  	}
-  	List<Integer> subList = new ArrayList<>();
-		for (String point : dataPoints) {
-			System.out.println( "> "+point );
-			if (!point.equals("")) subList.add( Integer.valueOf( point ) );
-		}
-//  	try {
-//			Files.write(dataFile.toPath(), (data+",").getBytes(), StandardOpenOption.APPEND);
-//			Files.write(dataFile.toPath(), "\n".getBytes(), StandardOpenOption.APPEND);
-//		} catch (Exception e) {
-//			System.out.println( "ERROR: couldn't write to "+dataFile );
-//			e.printStackTrace();
-//		}
-  	addDataList( subList );
-  }
-  
-  public void addDataList( List<Integer> dataList ) {
-  	pulses.add( dataList );
-  	if (pulses.size() > 100) pulses.remove(0);
-  	pulseTimes.add( LocalDateTime.now().toString() );
-  	if (pulseTimes.size() > 100) pulseTimes.remove(0);
   }
   
   private String arrayIntegerJoin (List<Integer> l) {
@@ -93,137 +50,159 @@ class PulseServerState extends ServerState {
   	for (Integer i : l) str += ","+i;
   	return str.substring(1);
   }
+  
+  private void addPulse ( int sessionId, PulseData pd ) {
+ 		Query q = pulseDatabase.query( sessionId );
+		//q.transform( "pulses", "Server Timestamp", "TimeStamp" )
+		q.input( "pulses", "Server Timestamp", "auto" )
+		 .input( "pulses", "Version", String.valueOf(pd.version()) )
+		 .input( "pulses", "Capture ID", String.valueOf(pd.captureId()) )
+		 .input( "pulses", "FPGA Count", String.valueOf(pd.count()) )
+		 .input( "pulses", "Server Count", String.valueOf(pd.actualCount()) )
+		 .input( "pulses", "FPGA Timestamp", String.valueOf(pd.timestamp()) )
+		 .input( "pulses", "Samples", arrayIntegerJoin( pd.samples() ) )
+		 .execute( true ); // write flag
+		System.out.println( q );
+	}
+	
+	public List<TableRow> pulseList ( int sessionId, String queryData ) {
+		Query q = pulseDatabase.query( sessionId );
+		q.execute( queryData );
+		List<TableRow> pulses = new ArrayList<>();
+		for (TableRow tr : q.rows( q.db().table("pulses"), "Server Timestamp" )) { // use "Server Timestamp" as the filter
+			pulses.add( tr );
+		}
+		return pulses;
+	}
 
+	
   public void respondHTTP ( RequestHTTP req, ResponseHTTP res ) {
   	System.out.println( req.path() );
-  	String latestPulse = pulses.size() > 0 ? arrayIntegerJoin(pulses.get(pulses.size()-1)) : "";
-  	String latestPulseTime = pulseTimes.size() > 0 ? pulseTimes.get(pulses.size()-1) : "";
-  	String plotlyStart =
-  		"<!DOCTYPE html>\n" +
-			"<head>\n" +
-//			"<meta charset=\"utf-8\" http-equiv=\"refresh\" content=\"1\" />\n" +
-//			"<style type=\"text/css\">\n" +
-//			"* {padding: 0;margin: 0;}\n" +
-//			"html, body {height: 100%;}\n" +
-//			"#container {min-height: 100%;background-color: #DDD;border-left: 2px solid #666;border-right: 2px solid #666;width: 676px;margin: 0 auto;}\n" +
-//			"* html #container {height: 100%;}\n" +
-//			"</style>\n" +
-			"</head>\n" +
-			"<body style='background-color:#eeeeee'>\n" +
-			"Latest pulse at: "+latestPulseTime+" <button onClick=\"window.open(encodeURI('data:text/csv;charset=utf-8,"+latestPulse+"'))\">Download CSV</button><br>\n" +
-			"<br><div id='plot_div'></div>\n" +
-			"<script src='https://cdn.plot.ly/plotly-latest.min.js'></script>\n" +
-			"<script>\n" +
-			"var layout = {\n" +
-    	" showlegend: false,\n" +
-			"	autosize: false,\n" +
-			"	width: 1200,\n" +
-			"	height: 720,\n" +
-			" scene: {aspectratio: {x:3, y:1, z:0.5}},\n" +
-//			"	yaxis: {\n" +
-//			"		title: 'Y-axis Title',\n" +
-//			"		automargin: true,\n" +
-//			"		titlefont: { size:30 },\n" +
-//			"	},\n" +
-//			"	paper_bgcolor: '#7f7f7f',\n" +
-//			"	plot_bgcolor: '#c7c7c7',\n" +
-			" barmode: 'overlay'\n" +
-			"};\n" ;
-			
-		String plotlyEnd = 
-			"</script>\n" +
-			"</body>\n" +
-			"</html>\n";
-			
-		String body = "";
 
-  	if ( req.socket().getLocalPort() == 49154 ) {
+		////////////////////////// plotly-latest.min.js
+  	if ( req.path().toLowerCase().equals("/plotly-latest.min.js") ) {
+			res.setBody( plotlyJS.toString() );
+			res.setMIME( "text/javascript" );
+
+		////////////////////////// 49154
+  	} else if ( req.socket().getLocalPort() == 49154 ) {
   		PulseData pd = new PulseData( req.data() );
-  		
-  		Query q = pulseDatabase.query( req.sessionId() );
-  		//q.transform( "pulses", "Server Timestamp", "TimeStamp" )
-  		q.input( "pulses", "Server Timestamp", "auto" )
-  		 .input( "pulses", "Version", String.valueOf(pd.version()) )
-  		 .input( "pulses", "Capture ID", String.valueOf(pd.captureId()) )
-  		 .input( "pulses", "FPGA Count", String.valueOf(pd.count()) )
-  		 .input( "pulses", "Server Count", String.valueOf(pd.actualCount()) )
-  		 .input( "pulses", "FPGA Timestamp", String.valueOf(pd.timestamp()) )
-  		 .input( "pulses", "Samples", pd.samples().toString() )
-  		 .execute();
-  		 
-  		addDataList( pd.samples() );
+  		addPulse( req.sessionId(), pd );
   		res.setMIME( "text/plain" );
   		res.setBody( "Converted string from hex to bytes:\n"+req.data()+"\n"+pd );
   		
+		////////////////////////// 49155
   	} else if ( req.socket().getLocalPort() == 49155 ) {
   		PulseData pd = new PulseData( req.data().getBytes() );
-  		addDataList( pd.samples() );
+  		addPulse( req.sessionId(), pd );
   		res.setMIME( "text/plain" );
   		res.setBody( "Thanks Storm!\n\n"+pd );
-  		
-  	} else if ( req.socket().getLocalPort() == 9000 ) {
-  		addDataCSV( req.data() );
+  		  		
+		////////////////////////// 2d
+  	} else if ( req.path().toLowerCase().equals("/2d") ) {
+   		List<TableRow> pulses = pulseList( req.sessionId(), req.data() );
+			String latestPulseTime = pulses.get(pulses.size()-1).read("Server Timestamp");
+			String latestPulse = pulses.get(pulses.size()-1).read("Samples");	
+			String plotlyDiv = "<table>\n";
+			String plotCode = "";
+			for (int i=pulses.size()-1; i>pulses.size()-11; i--) {
+				String divId = "plot"+i;
+				plotlyDiv += "<tr>"+
+										"<td width=100><button onClick=\"window.open(encodeURI('data:text/csv;charset=utf-8,"+pulses.get(i).read("Samples")+"'))\">Download CSV</button></td>"+
+										"<td width=100>"+pulses.get(i).read("Server Timestamp")+"</td>"+
+										"<td width=100>"+pulses.get(i).read("Version")+"</td>"+
+										"<td width=100>"+pulses.get(i).read("Capure ID")+"</td>"+
+										"<td width=100>"+pulses.get(i).read("FPGA Count")+"</td>"+
+										"<td width=100>"+pulses.get(i).read("Server Count")+"</td>"+
+										"<td width=100>"+pulses.get(i).read("FPGA Timestamp")+"</td>"+
+										"<td><div id='"+divId+"'></div></td>"+
+										"</tr>"+
+										"\n";
+				plotCode += "Plotly.newPlot( '"+divId+
+										"', [ {type:'scatter', marker:{opacity:'0.2', color:'#0000ff'}, y:["+pulses.get(i).read("Samples")+
+										"]} ], layout );\n";
+			}
+			plotlyDiv += "</table>";
   		res.setBody(
-  			"<form method='post'><br><br>\n" +
-  			"<input type='text' name='data'><br>\n" +
-  			"<input type='submit' value='Submit'>\n" +
-  			"</form><br><br>\n" +
-  			pulses
+  			plotlyTemplate
+				.replace( "latestPulseTime", latestPulseTime )
+				.replace( "latestPulse", latestPulse )
+  			.replace( "plotSize", "autosize:false, width:500, height:100," )
+  			.replace( "plotlyDiv", plotlyDiv )
+				.replace( "plotCode", plotCode
+				).toString()
   		);
-  		
-  	//} else if ( req.socket().getLocalPort() == 9001 ) {
-  	} else if ( req.path().toLowerCase().equals("/latest") ) {
-  		//System.out.println("9001");
-			body +=
-				plotlyStart +
-				"Plotly.newPlot( 'plot_div', [ \n";
-  		//if (pulses.size() > 0) {
-  			// only display latest pulse
-				body += "{type:'scatter', marker:{opacity:'0.2', color:'#0000ff'}, y:["+latestPulse+"]},\n";
-			//}
-			body +=
-				//" ], {\"barmode\":\"overlay\"} );\n" +
-				" ], layout );\n" +
-				plotlyEnd;
-  		res.setBody( body );
 			
-  	//} else if ( req.socket().getLocalPort() == 9002 ) {
-  	} else if ( req.path().toLowerCase().equals("/last100") ) {
-  		//System.out.println("9002");
-			body +=
-				plotlyStart +
-				"Plotly.newPlot( 'plot_div', [ \n";
-  		if (pulses.size() > 0) {
-				int x = 0;
-				for (List<Integer> pulse : pulses) {
+		////////////////////////// latest
+  	} else if ( req.path().toLowerCase().equals("/latest") ) {
+   		List<TableRow> pulses = pulseList( req.sessionId(), "pulses.Server+Timestamp.Last=" );  		
+  		String plotlyData = "";
+  		for (TableRow tr : pulses) {
+  			plotlyData += "{type:'scatter', marker:{opacity:'0.2', color:'#0000ff'}, y:["+tr.read( "Samples" )+"]},\n";
+			}			
+			String latestPulseTime = pulses.get(0).read("Server Timestamp");
+			String latestPulse = pulses.get(0).read("Samples");  		
+  		res.setBody(
+  			plotlyTemplate
+  			.replace( "plotlyDiv", "Latest pulse at: "+latestPulseTime+"<button onClick=\"window.open(encodeURI('data:text/csv;charset=utf-8,"+latestPulse+"'))\">Download CSV</button><br><br><div id='plot_div'></div>" )
+  			.replace( "plotSize", "autosize:false, width:1200, height:720," )
+				.replace( "latestPulseTime", latestPulseTime )
+				.replace( "latestPulse", latestPulse )
+				.replace( "plotCode",
+					"Plotly.newPlot( 'plot_div', [{type:'scatter', marker:{opacity:'0.2', color:'#0000ff'}, y:["+
+					latestPulse +
+					"]} ], layout );\n"
+				).toString()
+  		);
+
+ 		////////////////////////// 3d
+  	} else if ( req.path().toLowerCase().equals("/3d") ) {
+   		List<TableRow> pulses = pulseList( req.sessionId(), req.data() );
+  		String plotlyData = "";
+			String latestPulseTime = pulses.get(pulses.size()-1).read("Server Timestamp");
+			String latestPulse = pulses.get(pulses.size()-1).read("Samples");	
+ 			if (pulses.size() > 0) {
+ 				int x=0;
+				for (TableRow pulse : pulses) {
 					int y = 0;
 					String xStr = "";
 					String yStr = "";
-  				for (int i=0; i<pulse.size(); i++) {
-  					xStr += x+",";
-  					yStr += (y++)+",";
-  				}
+	 				for (int i=0; i<pulses.size(); i++) {
+//						xStr += "\""+pulse.read("Server Timestamp")+"\",";
+						xStr += x+",";
+						yStr += (y++)+",";
+					}
 					x++;
-					body += 	"{\n" +
-										"  x: ["+xStr+"],\n" +
-										"  y: ["+yStr+"],\n" +
-										"  z: "+pulse+",\n" +
-										"  marker: {\n" +
-										"	   size: 2,\n" +
-										"	   line: {\n" +
-										"	     width: 0\n" +
-										"    },\n" +
-										"    opacity: 0.5\n" +
-										"  },\n" +
-										"  type: 'scatter3d'\n" +
-										"},\n" ;
+					plotlyData += 	"{\n" +
+													"  x: ["+xStr+"],\n" +
+													"  y: ["+yStr+"],\n" +
+													"  z: ["+pulse.read("Samples")+"],\n" +
+													"  marker: {\n" +
+													"	   size: 2,\n" +
+													"	   line: {\n" +
+													"	     width: 0\n" +
+													"    },\n" +
+													"    opacity: 0.5\n" +
+													"  },\n" +
+													"  type: 'scatter3d'\n" +
+													"},\n" ;
 				}
 			}
-			body +=
-				" ], layout );\n" +
-				plotlyEnd;
-  		res.setBody( body );
+  		res.setBody(
+  			plotlyTemplate
+  			.replace( "plotlyDiv", "Latest pulse at: "+latestPulseTime+"<button onClick=\"window.open(encodeURI('data:text/csv;charset=utf-8,"+latestPulse+"'))\">Download CSV</button><br><br><div id='plot_div'></div>" )
+  			.replace( "plotSize", "autosize:false, width:1200, height:720," )
+				.replace( "latestPulseTime", latestPulseTime )
+				.replace( "latestPulse", latestPulse )
+				.replace( "plotCode",
+					"Plotly.newPlot( 'plot_div', [ \n" +
+					plotlyData +
+					" ], layout );\n"
+				).toString()
+  		);
+
   	}
+  	
   }
 
 }
